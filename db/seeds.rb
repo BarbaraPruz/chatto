4199f97cd6318e9919394817f4d4ec27d2f08c14
BarbaraPruz <barbara.prusiewicz@gmail.com>
2019-01-05 21:04:21 -0800

    Add Room model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 u1 = User.create(:screen_name => "Lou", :email => "lou@domain.com", :password => "test")
 u2 = User.create(:screen_name => "admin", :email => "admin@domain.com", :password => "test", :admin => "true")
 u3 = User.create(:screen_name => "Bud", :email => "bud@domain.com", :password => "test")
+
+r1 = Room.create(:name => "General")
+r2 = Room.create(:name => "Sports")
+r3 = Room.create(:name => "Travel")

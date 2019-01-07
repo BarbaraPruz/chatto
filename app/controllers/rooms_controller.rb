df13fc32de540f8bb098c46e466155412ad224de
BarbaraPruz <barbara.prusiewicz@gmail.com>
2019-01-06 17:39:02 -0800

    Add Join room

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,11 @@
+class RoomsController < ApplicationController
+    before_action :require_logged_in
+
+    def show
+        @room=Room.find_by(:id => params[:id])
+    end
+
+
+    private
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
 
   resources :users
   
+  # rooms and messages
+  resources :rooms
+  
 end

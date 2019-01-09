230664ecdf5eabd9164d00cfc98601c20e05e131
BarbaraPruz <barbara.prusiewicz@gmail.com>
2019-01-08 16:59:42 -0800

    Add message to room

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,16 @@ class MessagesController < ApplicationController
     before_action :require_logged_in
 
     def create
-        puts "New Message #{params}"
-        # verify params id matches current user
-        # if save message ok, go to show room?
-        # else show error
+        # validate user matches current user?
+        message = Message.create(message_params) 
+        # if invalid show message?
+      ##  puts "Error! #{message.errors.full_messages}" if !message.valid?
+        redirect_to room_path(:id => params[:message][:room_id])
     end        
 
     private
-
+        
+    def message_params
+        params.require(:message).permit(:content, :room_id, :user_id)
+    end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
     validates :content, presence: true
-    validates :room_id, presence: true
-    validates :user_id, presence: true
+    validates :room, presence: true
+    validates :user, presence: true
 
     belongs_to :room
     belongs_to :user    
